4c2be705a470fbdfa990c5f63cc1ccfc80be127c
pinair <riccardo.sibani@gmail.com>
2017-09-15 17:46:56 +0200

    Ass 6 done

--- dectrees/python/pruning.py
+++ dectrees/python/pruning.py
@@ -1,5 +1,6 @@
 import monkdata as m
 import dtree as dtree
+import drawtree_qt5 as drawLib
 import random
 
 def partition(data, fraction):
@@ -8,20 +9,27 @@ def partition(data, fraction):
     breakPoint = int(len(ldata) * fraction)
     return ldata[:breakPoint], ldata[breakPoint:]
 
-monk1train, monk1val = partition(m.monk1, 0.6)
 
-# Rebuild the tree
-decisionTree = dtree.buildTree(m.monk1, m.attributes)
 
-print decisionTree
 
-prunedTrees = dtree.allPruned(decisionTree)
 
-for local in prunedTrees:
-    print local
-    print  "\n"
-    for sample in monk1val:
-        print dtree.classify(local, sample)
+for i in range(0,1000000):
+    monk1train, monk1val = partition(m.monk1, 0.6)
+    # Rebuild the tree
+    decisionTree = dtree.buildTree(monk1train, m.attributes)
+    prunedTrees = dtree.allPruned(decisionTree)
+    for local in prunedTrees:
+        print local
+        print  "\n"
+        print "check = ", dtree.check(local, monk1train)
+        print "check = ", dtree.check(local, monk1val)
+        if dtree.check(local, monk1val) == 1.0:
+            print local
+            print "Check test: ", dtree.check(local, m.monk1test) # 0.944444444444
+            drawLib.drawTree(local)
+            #for sample in monk1train:
+            #    print "class = ", dtree.classify(local, sample)
+
 
 # for x in range(0,6) {
 #     pruned =  dtree.allPruned(decisionTree)
