7c116465bc1d919bb62fd4c2b9ea4f6e4d7884b2
pinair <riccardo.sibani@gmail.com>
2017-09-15 22:13:54 +0200

    Ass 6 done

--- dectrees/python/pruning.py
+++ dectrees/python/pruning.py
@@ -10,25 +10,40 @@ def partition(data, fraction):
     return ldata[:breakPoint], ldata[breakPoint:]
 
 
+monk1train, monk1val = partition(m.monk1, 0.6)
+decisionTree = dtree.buildTree(monk1train, m.attributes)
 
 
+def myPrune(tree):
+    goalCheck = dtree.check(tree, monk1val)
+    
+    newPrunes = dtree.allPruned(tree)
+    for prunedTree in newPrunes:
+        if(goalCheck < dtree.check(prunedTree, monk1val)):
+            myPrune(prunedTree)
+    
+    print "Check ", goalCheck
+    print "TEST CHECK ", dtree.check(tree, m.monk1test)
+    drawLib.drawTree(tree)
 
-for i in range(0,1000000):
-    monk1train, monk1val = partition(m.monk1, 0.6)
-    # Rebuild the tree
-    decisionTree = dtree.buildTree(monk1train, m.attributes)
-    prunedTrees = dtree.allPruned(decisionTree)
-    for local in prunedTrees:
-        print local
-        print  "\n"
-        print "check = ", dtree.check(local, monk1train)
-        print "check = ", dtree.check(local, monk1val)
-        if dtree.check(local, monk1val) == 1.0:
-            print local
-            print "Check test: ", dtree.check(local, m.monk1test) # 0.944444444444
-            drawLib.drawTree(local)
-            #for sample in monk1train:
-            #    print "class = ", dtree.classify(local, sample)
+myPrune(decisionTree)
+
+# for i in range(0,1000000):
+#     monk1train, monk1val = partition(m.monk1, 0.6)
+#     # Rebuild the tree
+#     decisionTree = dtree.buildTree(monk1train, m.attributes)
+#     prunedTrees = dtree.allPruned(decisionTree)
+#     for local in prunedTrees:
+#         print local
+#         print  "\n"
+#         print "check = ", dtree.check(local, monk1train)
+#         print "check = ", dtree.check(local, monk1val)
+#         if dtree.check(local, monk1val) == 1.0:
+#             print local
+#             print "Check test: ", dtree.check(local, m.monk1test) # 0.972222222222
+#             drawLib.drawTree(local)
+#             #for sample in monk1train:
+#             #    print "class = ", dtree.classify(local, sample)
 
 
 # for x in range(0,6) {
